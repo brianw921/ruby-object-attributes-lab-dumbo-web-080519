a75911035307a5a83f23800d1f84bab26980b0d4
Brian Wong <bwong92@gmail.com>
2019-08-11 18:52:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,2 @@
-class Dog
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
+class Person
 end
